8c577e29f5e9082082c986a29a209ab347b473c9
borz93 <borja.sanchez01@gmail.com>
2016-06-05 19:19:36 +0200

    feat: add article tag to preview view

--- resources/views/frontend/articles/layouts/previews/article.blade.php
+++ resources/views/frontend/articles/layouts/previews/article.blade.php
@@ -1,20 +1,22 @@
-<div class="col-md-6 same-height">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h1 class="text-center text-warning post-title">
-                {{link_to('articulos/'.$article->product->slug.'/'.$article->slug,$article->title,['class'=>''])}}
-            </h1>
-            <div class="text-center">
-                <img src="{{ url("image/cache/original/".$article->file->name) }}" class="img-responsive image-preview-products">
+<article id="{{$article->id}}">
+    <div class="col-md-6 same-height">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h1 class="text-center text-warning post-title">
+                    {{link_to('articulos/'.$article->product->slug.'/'.$article->slug,$article->title,['class'=>''])}}
+                </h1>
+                <div class="text-center">
+                    <img src="{{ url("image/cache/original/".$article->file->name) }}" class="img-responsive image-preview-products">
+                </div>
             </div>
-        </div>
-        <div class="panel-body">
-            <div class="custom-text">
-                {!! $article->intro !!}
+            <div class="panel-body">
+                <div class="custom-text">
+                    {!! $article->intro !!}
+                </div>
             </div>
-        </div>
-        <div class="panel-footer">
-            {{link_to('articulos/'.$article->product->slug.'/'.$article->slug,'Entrar',['class'=>'btn btn-indigo'])}}
+            <div class="panel-footer">
+                {{link_to('articulos/'.$article->product->slug.'/'.$article->slug,'Entrar',['class'=>'btn btn-indigo'])}}
+            </div>
         </div>
     </div>
-</div>
+</article>
