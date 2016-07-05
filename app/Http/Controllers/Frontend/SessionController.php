1df3d1261d2ee31fa2739e22d837d312ea2b4cf4
borz93 <borja.sanchez01@gmail.com>
2016-07-05 22:00:41 +0200

    no message

--- app/Http/Controllers/Frontend/SessionController.php
+++ app/Http/Controllers/Frontend/SessionController.php
@@ -13,19 +13,22 @@ class SessionController extends Controller
     public function show($genre,$session)
     {
         $session = Session::findBySlugOrFail($session);
-        SEO::setDescription(str_limit($session->body,150));
-        SEO::metatags()->addMeta('article:published_time', $session->created_at->toW3CString(), 'property');
-        SEO::metatags()->addMeta('article:section','música','property');
-        SEO::metatags()->addKeyword($session->tags);
-
-        SEO::opengraph()->setTitle($session->title)
-            ->addImage(url("image/cache/original/".$session->file->name))
-            ->setArticle([
-                'published_time' => $session->created_at->toW3CString(),
-                'modified_time' => $session->updated_at->toW3CString(),
-                'author' => $session->user->name,
-                //'tag' => 'string / array'
-            ]);
-        return view('frontend.sessions.session',compact('session'));
+        if($session->genre == $genre){
+            SEO::setDescription(str_limit($session->body,150));
+            SEO::metatags()->addMeta('article:published_time', $session->created_at->toW3CString(), 'property');
+            SEO::metatags()->addMeta('article:section','música','property');
+            SEO::metatags()->addKeyword($session->tags);
+            SEO::opengraph()->setTitle($session->title)
+                ->addImage(url("image/cache/original/".$session->file->name))
+                ->setArticle([
+                    'published_time' => $session->created_at->toW3CString(),
+                    'modified_time' => $session->updated_at->toW3CString(),
+                    'author' => $session->user->name,
+                    //'tag' => 'string / array'
+                ]);
+            return view('frontend.sessions.session',compact('session'));
+        }else{
+            abort(404);
+        }
     }
 }
