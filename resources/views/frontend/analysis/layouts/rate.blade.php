a96339ed552b30b8cb0b3c786f7549f9e894b4d0
borz93 <borja.sanchez01@gmail.com>
2016-08-11 19:48:08 +0200

    feat: Add style to rate number in analisis

--- resources/views/frontend/analysis/layouts/rate.blade.php
+++ resources/views/frontend/analysis/layouts/rate.blade.php
@@ -3,8 +3,10 @@
         <div class="panel-heading  panel-heading-custom-title">
             <h5>Puntuación</h5>
         </div>
-        <div class="panel-body">
-            {{$analysi->vote}}
+        <div class="panel-body vote-number">
+            <p class="">{{$analysi->vote}}</p>
+            <hr align="center">
+            <h2 class="">10</h2>
         </div>
     </div>
 </div>
